c11d74d144faa7809a8976e52c8ba982a40dc49d
abr1d <42777867+abr1d@users.noreply.github.com>
2018-12-15 00:15:12 -0500

    Update hello_SLURM.sh

--- source/hello_SLURM.sh
+++ source/hello_SLURM.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #SBATCH -t 1:00:00
 #SBATCH -N 12
@@ -7,13 +8,16 @@ set -x
 
 echo $SLURM_SUBMIT_DIR
 
-module load mpi/gcc_openmpi
+# module load mpi/gcc_openmpi
 
 cd /scratch
-mpicc hello.c
+/software/openmpi/3.1.2/bin/mpicc  /scratch/pi_mc.c -lm
+
+sudo touch output.txt
+sudo chmod 777 output.txt
 
 for i in {2..12..2}
 do
   echo "With ${i} processes"
-  time mpirun --allow-run-as-root -np $i ./a.out
+  time /software/openmpi/3.1.2/bin/mpirun -np $i --hostfile ./machine_list ./a.ou$
 done
